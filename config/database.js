11fb662332cb60526367a7b98ed2ffa5005e4a5f
Ruddy Cruz <rudolph555.dev@gmail.com>
2024-09-28 16:49:34 -0400

    datos de la bd modif

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
 const { Sequelize } = require('sequelize');
 
 // Define las variables de configuración
-const dbName = 'bd281_GIT';       // Nombre de la base de datos
+const dbName = 'postgres';       // Nombre de la base de datos
 const dbUser = 'postgres';       // Nombre de usuario
-const dbPassword = '123456789';
+const dbPassword = '123';
 const dbHost = 'localhost';      // Host de la base de datos
 
 // Configura la conexión a la base de datos
